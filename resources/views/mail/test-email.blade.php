64330f81fe70de6cc62a883643b3fd0c1d896e3f
Hamza <tp@imangu.com>
2024-01-29 17:28:37 +0500

    eosvolt email template: add yeild and section

--- resources/views/mail/test-email.blade.php
+++ resources/views/mail/test-email.blade.php
@@ -4,167 +4,163 @@
 @section('heading', 'Welcome to EOSVolt')
 
 @section('body')
-
-
-    <tr>
-        <td style="width: 24px; background-color: #F3F4F8">&nbsp;</td>
-        <td style="width: 24px; background-color: #ffffff">&nbsp;</td>
-        <td style="background-color: #ffffff; width: 564px; color: #000000; font-size: 14px; font-style: normal">
-
-            <table style="height: 60px; border: unset; border-collapse: collapse;">
-                <tr>
-                    <td>
-                        &nbsp;
-                    </td>
-                </tr>
-
-
-                <tr>
-                    <td><p>Hello,</p></td>
-                </tr>
-
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                    </td>
-                </tr>
-
-                <tr>
-                    <td><p>You are invited by your colleague to join the AeroGuard account of , to gain direct access to real-time air quality data.</p></td>
-                </tr>
-
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                    </td>
-                </tr>
-
-                <tr>
-                    <td><p>Click below to accept and create your own MyPlymovent account.</p></td>
-                </tr>
-
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        <a style="display: inline-block; box-sizing: border-box; border-radius: 4px; min-width: 240px; text-decoration: none; background-color: #FFCB1F; text-align: center; color: #000000; padding: 12px 5px;
-                        font-size: 16px; font-style: normal; font-weight: 700; text-decoration: unset" href="#">&nbsp;&nbsp;&nbsp;Accept&nbsp;&nbsp;&nbsp;</a>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        <p style="font-style: italic">Thank You, </p>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        &nbsp;
-                        <p style="font-style: italic">The Plymovent Team</p>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                        <br />
-                    </td>
-                </tr>
-
-            </table>
-
-        </td>
-        <td style="width: 24px; background-color: #ffffff">&nbsp;</td>
-        <td style="width: 24px; background-color: #F3F4F8">&nbsp;</td>
-    </tr>
-
-
+    <table style="width: 612px; margin: 0 auto; border: unset; border-collapse: collapse;">
+        <tr style="background-color: #F0FCFD">
+            <td colspan="5" style="height: 64px">
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                <table>
+                    <tr style="background-color: #F0FCFD;">
+                        <td style="width: 50%;">&nbsp;</td>
+                        <td>
+                            <img src="{{ asset('images/eosvolt.png') }}" alt="eosvolt" />
+                        </td>
+                        <td style="width: 50%;">&nbsp;</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD; margin: 0 auto; text-align: center;">
+            <td>
+                <h2 style="color: black">Welcome</h2>
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                <p style="color: black; text-align: center; line-height: 26px">Welcome to EOSVolt! We're excited to have you <br> join our community of EV enthusiasts. To finalize <br> your registration and gain access to all the <br> amazing features of our app, we just need one <br> more step: confirming your email.</p>
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                <p style="font-size: 16px; font-weight: 700; color: black; text-align: center">Here is your confirmation code:</p>
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                <table>
+                    <tr>
+                        <td style="width: 50%;">&nbsp;</td>
+                        <td>
+                            <a style="display: inline; box-sizing: border-box; border-radius: 8px; text-decoration: none; background-color: white; text-align: center; color: #000000; font-size: 16px; font-style: normal; font-weight: 700; text-decoration: none !important; text-underline: none !important;" href="#">
+                                <table style="background-color: white; border: unset; border-collapse: separate; border-spacing: 0 !important; border-radius: 8px; width: 178px">
+                                    <tr style="background-color: white; border-radius: 8px;">
+                                        <td colspan="3" style="height: 8px; line-height: 8px; border-top-left-radius: 8px; border-top-right-radius: 8px; background-color: white; padding-top: 4px; border-top: 1px solid black; border-left: 1px solid black; border-right: 1px solid black;">
+                                            &nbsp;
+                                        </td>
+                                    </tr>
+                                    <tr style="background-color: white;">
+                                        <td style="background-color: white; border-left: 1px solid black;">&nbsp; &nbsp;</td>
+                                        <td style="background-color: white; text-align: center">
+                                            <a style="color: #000000; font-size: 40px; text-decoration: none !important; text-underline: none !important; font-weight: 700;" href="#">230051</a>
+                                        </td>
+                                        <td style="background-color: white; border-right: 1px solid black;">&nbsp; &nbsp;</td>
+                                    </tr>
+                                    <tr style="background-color: white; border-radius: 4px;">
+                                        <td colspan="3" style="height: 8px; line-height: 8px; border-bottom-left-radius: 8px; border-bottom-right-radius: 8px; background-color: white; padding-bottom: 4px; border-bottom: 1px solid black; border-left: 1px solid black; border-right: 1px solid black;">
+                                            &nbsp;
+                                        </td>
+                                    </tr>
+                                </table>
+                            </a>
+                        </td>
+                        <td style="width: 50%;">&nbsp;</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                <p style="color: black; text-align: center">Enter the above code in EOSVolt app to complete sign up </p>
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr style="background-color: #F0FCFD;">
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+    </table>
 @endsection
 
-@section('emailBottomDescription')
-
-
-    <tr style="background-color: #F3F4F8">
-        <td style="width: 24px;">&nbsp;</td>
-        <td style="width: 24px;">&nbsp;</td>
-        <td style="width: 564px; font-size: 12px; font-weight: 400; color: #1e453eb3">
-            <table>
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                        <br />
-                        <br />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p style="text-align: center">If you do not want to create an account, just ignore this email. Do not reply to this email as it will not be received. If you need assistance, please visit us at <a style="color: rgba(38, 38, 38, 0.70); font-size: 12px; font-style: normal; font-weight: 700; line-height: 20px; text-decoration-line: underline;" href="https://www.plymovent.com/en/contact-us">Plymovent Support.</a>.</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        &nbsp;
-                        <br />
-                        <br />
-                        <br />
-                        <br />
-                        <br />
-                    </td>
-                </tr>
-            </table>
-        </td>
-        <td style="width: 24px;">&nbsp;</td>
-        <td style="width: 24px;">&nbsp;</td>
-    </tr>
-
-@endsection
