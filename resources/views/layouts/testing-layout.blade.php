2bec4acb00a3e152625ec896d1aa662006d3d09c
Hamza <tp@imangu.com>
2024-01-29 11:28:58 +0500

    complete eosvolt email template

--- resources/views/layouts/testing-layout.blade.php
+++ resources/views/layouts/testing-layout.blade.php
@@ -28,9 +28,150 @@
             &nbsp;
         </td>
     </tr>
-    <tr style="background-color: #F0FCFD; width: 100%">
-        <td style="width: 612px;">
-            <img src="../../js/assets/images/eosvolt.png" alt="logo" />
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            <table>
+                <tr style="background-color: #F0FCFD;">
+                    <td style="width: 50%;">&nbsp;</td>
+                    <td>
+                        <img src="{{ asset('images/eosvolt.png') }}" alt="eosvolt" />
+                    </td>
+                    <td style="width: 50%;">&nbsp;</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD; margin: 0 auto; text-align: center;">
+        <td>
+            <h2 style="color: black">Welcome</h2>
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            <p style="color: black; text-align: center; line-height: 26px">Welcome to EOSVolt! We're excited to have you <br> join our community of EV enthusiasts. To finalize <br> your registration and gain access to all the <br> amazing features of our app, we just need one <br> more step: confirming your email.</p>
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            <p style="font-size: 16px; font-weight: 700; color: black; text-align: center">Here is your confirmation code:</p>
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            <table>
+                <tr>
+                    <td style="width: 50%;">&nbsp;</td>
+                    <td>
+                        <a style="display: inline; box-sizing: border-box; border-radius: 8px; text-decoration: none; background-color: white; text-align: center; color: #000000; font-size: 16px; font-style: normal; font-weight: 700; text-decoration: none !important; text-underline: none !important;" href="#">
+                            <table style="background-color: white; border: unset; border-collapse: separate; border-spacing: 0 !important; border-radius: 8px; width: 178px">
+                                <tr style="background-color: white; border-radius: 8px;">
+                                    <td colspan="3" style="height: 8px; line-height: 8px; border-top-left-radius: 8px; border-top-right-radius: 8px; background-color: white; padding-top: 4px; border-top: 1px solid black; border-left: 1px solid black; border-right: 1px solid black;">
+                                        &nbsp;
+                                    </td>
+                                </tr>
+                                <tr style="background-color: white;">
+                                    <td style="background-color: white; border-left: 1px solid black;">&nbsp; &nbsp;</td>
+                                    <td style="background-color: white; text-align: center">
+                                        <a style="color: #000000; font-size: 40px; text-decoration: none !important; text-underline: none !important; font-weight: 700;" href="#">230051</a>
+                                    </td>
+                                    <td style="background-color: white; border-right: 1px solid black;">&nbsp; &nbsp;</td>
+                                </tr>
+                                <tr style="background-color: white; border-radius: 4px;">
+                                    <td colspan="3" style="height: 8px; line-height: 8px; border-bottom-left-radius: 8px; border-bottom-right-radius: 8px; background-color: white; padding-bottom: 4px; border-bottom: 1px solid black; border-left: 1px solid black; border-right: 1px solid black;">
+                                        &nbsp;
+                                    </td>
+                                </tr>
+                            </table>
+                        </a>
+                    </td>
+                    <td style="width: 50%;">&nbsp;</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            <p style="color: black; text-align: center">Enter the above code in EOSVolt app to complete sign up </p>
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
+        </td>
+    </tr>
+    <tr style="background-color: #F0FCFD;">
+        <td>
+            &nbsp;
         </td>
     </tr>
 
