e80e79e9cd7578258bb31f30304c1a53baaa4883
HamzaKhan <hamzakhan17826@gmail.com>
2024-02-17 20:55:26 +0500

    add offset and limit logic which is commented

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,22 +10,25 @@ class DashboardController extends Controller
 {
     public function index(){
         info('ContaminateController.index');
+//        $perPage = 1;
+//        $page = $request->current_page ?? 1;
+//        $offset = ($page-1)*$perPage;
+//        $contaminate = Contaminate::offset($offset)->limit($perPage)->get();
         $contaminate = Contaminate::paginate(1);
         return Inertia::render('dashboard', compact('contaminate'));
     }
     public function dashboardContaminate(Request $request){
         info('ContaminateController.index');
+        $contaminate = '';
         if ($request->status == 'prev'){
             $request->current_page = $request->current_page - 1;
             $contaminate = Contaminate::paginate(1, ['*'], 'page', $request->current_page);
-            return response()->json(['status' => 'success', 'data' => $contaminate]);
         }else if($request->status == 'next'){
             $request->current_page = $request->current_page + 1;
             $contaminate = Contaminate::paginate(1, ['*'], 'page', $request->current_page);
-            return response()->json(['status' => 'success', 'data' => $contaminate]);
         }else if($request->current_page){
             $contaminate = Contaminate::paginate(1, ['*'], 'page', $request->current_page);
-            return response()->json(['status' => 'success', 'data' => $contaminate]);
         }
+        return response()->json(['status' => 'success', 'data' => $contaminate]);
     }
 }
